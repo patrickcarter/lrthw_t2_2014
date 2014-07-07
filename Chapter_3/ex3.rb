6fc68c2144121f4d0aedde5c7aa7ec46001a545f
patrick Carter <patrickcarter1@yahoo.com>
2014-07-06 21:33:54 -0600

    Added updates

--- Chapter_3/ex3.rb
+++ Chapter_3/ex3.rb
@@ -1,15 +1,15 @@
 puts 'I will now count my chickens.'
 # below is the formula to find out how many hens there are
 puts 'Hens', 25.0 + 30 / 6
-puts 'Roosters', 100 - 25 * 3 % 4
+puts 'Roosters', 100.0 - 25.0 * 3.0 % 4.0
 # print the string
 puts 'Now I will count the eggs'
 # is total number of eggs
-puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1 / 4 + 6
 # is asking if that equation is true
 puts 'Is it true that 3 + 2 < 5 - 7?'
 # is the correct way to write the equation
-puts 3 + 2 < 5 - 7
+puts 3.0 + 2.0 < 5.0 - 7.0
 # will print the string then print the value
 puts 'What is 3 + 2?', 3 + 2
 puts 'What is 5 - 7?', 5 - 7
