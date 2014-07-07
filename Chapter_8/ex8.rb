22c04718b7234db1aff910da44b7d59afbab4f96
patrick Carter <patrickcarter1@yahoo.com>
2014-07-07 08:10:14 -0600

    Added updates

--- Chapter_8/ex8.rb
+++ Chapter_8/ex8.rb
@@ -1,12 +1,12 @@
 formatter = '%s %s %s %s'
 
 puts formatter % [1, 2, 3, 4]
-puts formatter % ["one", "two", "three", "four"]
+puts formatter % ['"one", "two", "three", "four"']
 puts formatter % [true, false, false, true]
 puts formatter % [formatter, formatter, formatter, formatter]
 puts formatter % [
-     'I had this thing.',
-     'That you could type up right.',
-     "'But it didnt't sing.'",
-     'So I said goodnight'
+  'I had this thing.',
+  'That you could type up right.',
+  "'But it didnt't sing.'",
+  'So I said goodnight'
 ]
