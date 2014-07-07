a80d95c63fa04c6ee4b29c198a24547b503e6716
patrick Carter <patrickcarter1@yahoo.com>
2014-07-06 21:32:56 -0600

    Added updates

--- Chapter_2/ex2.rb
+++ Chapter_2/ex2.rb
@@ -3,7 +3,7 @@
 
 puts 'I could have code like this.' # and this comment will ignored.
 
-# You can also use a "comment" to disable or comment out a piece of code.
+# You can also use a comment to "disable" or comment out a piece of code:
 # puts "This wont run."
 
 puts 'this will run.'
