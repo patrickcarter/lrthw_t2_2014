e7877dcfd72df096ee62186318d62aaee70a14ba
patrick Carter <patrickcarter1@yahoo.com>
2014-07-16 05:28:03 -0600

    added

--- Chapter_2/ex2.rb
+++ Chapter_2/ex2.rb
@@ -1,7 +1,7 @@
 # A comment, this is so you can read your program later.
 # Anything after # is ignored by Ruby
 
-puts 'I could have code like this.' # and this comment will ignored.
+puts "I could have code like this." # and the comment after is ignored
 
 # You can also use a comment to "disable" or comment out a piece of code:
 # puts "This wont run."
