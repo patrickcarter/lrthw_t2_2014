306651771aa3618222fcc640544aabebfe8bc405
patrick Carter <patrickcarter1@yahoo.com>
2014-08-04 08:38:18 -0600

    trying this again

--- Chapter_2/ex2.rb
+++ Chapter_2/ex2.rb
@@ -1,9 +1,9 @@
 # A comment, this is so you can read your program later.
-# Anything after # is ignored by Ruby
+# Anything after the # is ignored by Ruby.
 
-puts "I could have code like this." # and the comment after is ignored
+puts 'I could have code like this.' # and the comment after is ignored
 
 # You can also use a comment to "disable" or comment out a piece of code:
-# puts "This wont run."
+# puts "This won't run."
 
-puts "This wont run"
+puts 'This will run.'
